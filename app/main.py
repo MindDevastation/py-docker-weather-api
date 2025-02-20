d87468b3201c5db1f312b5b69c9858af8b94b6d7
Aleksey Pastukhov <mindflux99@gmail.com>
2025-02-20 11:20:14 +0200

    Added changes

--- app/main.py
+++ app/main.py
@@ -4,11 +4,12 @@ import requests
 
 
 def get_weather(city: str) -> None:
+    WEATHER_API_URL = "https://api.weatherapi.com/v1/current.json"
     api_key = os.getenv("API_KEY")
     if not api_key:
         raise Exception("API_KEY environment variable not set")
 
-    url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={city}"
+    url = f"{WEATHER_API_URL}?key={api_key}&q={city}"
     response = requests.get(url)
 
     if response.status_code == 200:
@@ -20,7 +21,8 @@ def get_weather(city: str) -> None:
     else:
         print(f"Failed to get weather data: "
               f"{response.status_code}, "
-              f"{response.text}")
+              f"{response.text},"
+              f"{city}")
 
 
 if __name__ == "__main__":
